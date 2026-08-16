403ecf3e24b537a6d4c0bc0275429a680eead4e7
Daniel Granato <dgmgranato@gmail.com>
2026-08-16 12:53:47 -0300

    feat: allow task deselection with early return

--- script-crud.js
+++ script-crud.js
@@ -51,6 +51,10 @@ function criarElementoTarefa(tarefa) {
     li.append(botao)
 
     li.onclick = () => {
+        if (li.classList.contains('app__section-task-list-item-active')) {
+        li.classList.remove('app__section-task-list-item-active')
+        return
+        }
         const tarefaAtiva = document.querySelector(".app__section-task-list-item-active")
         if (tarefaAtiva) {
             tarefaAtiva.classList.remove("app__section-task-list-item-active")
